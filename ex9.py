80602e6de0455f451317dbcfa54ce5ee1619f067
869100359@qq.com <deculare@gmail.com>
2019-07-21 10:53:36 +0800

    Added ex9.py

---
+++ ex9.py
@@ -0,0 +1,16 @@
+#Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months1 =r"Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+months2 ="Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days:",days)
+print("Here are the months path:",months1)
+print("Here are the months:",months2)
+
+print("""
+There's something going on here.
+With the threee double-qutoes.
+We'll be able to type as musch as we like.
+Even 4 lines if we want, or 5,or 6.
+""")
