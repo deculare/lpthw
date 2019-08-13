3bbaf7ca8d4f5d5bca6fe9806d3c98db71e76931
deculare <869100359@qq.com>
2019-08-14 05:52:55 +0800

    Added ex0025.py

---
+++ ex0025.py
@@ -0,0 +1,6 @@
+import ex25
+sentence=input('Please input a sentence for manipulate ')
+sorted_words = ex25.sort_sentence(sentence)
+print(sorted_words)
+ex25.print_first_and_last(sentence)
+ex25.print_first_and_last_sorted(sentence)
